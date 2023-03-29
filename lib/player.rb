a4acc7147d3be193c9893b11dc10a17de6e11ee5
Javen Wilson <javenb022@gmail.com>
2023-03-29 11:41:52 -0400

    add total cost method

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,10 @@
 class Player
-  attr_reader
+  attr_reader :monthly_cost, :contract_length, :nickname
   def initialize(name, monthly_cost, contract_length)
     @name = name
     @monthly_cost = monthly_cost
     @contract_length = contract_length
+    @nickname = nil
   end
 
   def first_name
@@ -14,5 +15,9 @@ class Player
   def last_name
     name_s = @name.split(" ")
     name_s.last
+  end
+
+  def total_cost
+    contract_length * monthly_cost
   end
 end
