6e4937caffacd4d68f1e2bff5d2e368dedad3e49
Javen Wilson <javenb022@gmail.com>
2023-03-29 12:54:31 -0400

    add methods I couldn't finish

--- lib/team.rb
+++ lib/team.rb
@@ -22,4 +22,19 @@ class Team
   def short_term_players
     @roster.select {|player| player.contract_length <= 24}
   end
+
+  # couldn't get method to work vv
+  def total_value
+    # require 'pry'; binding.pry
+    total = @roster.select do |player| 
+      player.total_cost
+    end
+  end
+
+  # Couldn't get method to work in time
+  def players_by_last_name
+    @roster.select do |player|
+      player.last_name.sort
+    end
+  end
 end
