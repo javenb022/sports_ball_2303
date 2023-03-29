2d4297adda2d1937653f2159aafa1b0259052bed
Javen Wilson <javenb022@gmail.com>
2023-03-29 11:53:34 -0400

    add working iteration 1 tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -37,4 +37,11 @@ RSpec.describe Player do
 
     expect(player.total_cost).to eq(36000000)
   end
+
+  it 'has a nickname' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    expect(player.nickname).to eq(nil)
+    expect(player.nickname).to eq("Squints")
+  end
 end
