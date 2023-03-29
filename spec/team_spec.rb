5e585912e98fe7eab6ce7d4fb6b6c54fe2f37a40
Javen Wilson <javenb022@gmail.com>
2023-03-29 12:55:02 -0400

    add tests that I started but didn't finish

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -64,4 +64,55 @@ RSpec.describe Team do
 
     expect(team.short_term_players).to eq([player_2, player_4])
   end
+
+  xit 'checks for team total value' do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+    # require 'pry'; binding.pry
+    expect(team.total_value).to eq(85200000)
+  end
+
+  xit 'checks for team details' do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.details).to eq()
+  end
+
+  it 'checks for players sorted alphabetically' do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    # expect(team.average_cost_of_plyaer).to eq("21,300,000")
+    expect(team.players_by_last_name).to eq("DeNunez, McClennan, Palledorous, Porter")
+  end
+
+
 end
